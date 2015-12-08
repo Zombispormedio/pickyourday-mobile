031bbda129df048145d1e1ff75d0571ecce14922
Claudia Pascual Gea <clau.p.gea@gmail.com>
2015-12-08 18:48:11 +0100

    Modals and login

--- www/app.js
+++ www/app.js
@@ -1,9 +1,42 @@
 // Ionic Starter App
 
+var adminCtrl = {};
+var adminFtry = {};
+var adminFiltr = {};
+var adminDrctv = {};
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
+  .controller(adminCtrl)
+  .factory(adminFtry)
+  .filter(adminFiltr)
+  .directive(adminDrctv)
+
+  .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+
+  $stateProvider
+      .state("login", {
+        url: "/login",
+        onEnter: function ($rootScope) {
+          if (getJSONLocal("user")) {            
+            $rootScope.go("app");
+          }
+        },
+        templateUrl: 'app/login/main.html',
+        controller: 'LoginCtrl'
+
+      })
+
+    $urlRouterProvider.otherwise("/login");
+    $httpProvider.interceptors.push('AuthInterceptor');
+
+  })
+
+
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +50,5 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+
---
+++ www/app/login/ctrl.js
@@ -0,0 +1,51 @@
+adminCtrl.LoginCtrl = function ($rootScope, $scope, $http, $ionicModal) {
+	$scope.error="";
+	$scope.user = {};
+	$scope.login = function () {
+		if ($scope.user.email !== "" && $scope.user.password !== "") {
+			$http.post("http://pickyourday.herokuapp.com/api/oauth", $scope.user).then(function successCallback(response) {
+				var res = response.data;
+				if (!res.error) {
+					saveLocal("user", response.data.data);
+					$rootScope.go("app");
+				} else {
+					$scope.error=res.error;
+					$scope.openModal();
+				}
+
+			}, function errorCallback(response) {
+
+			});
+		}
+
+	}
+	
+	$scope.cleanError=function(){
+			$scope.error="";
+	}
+
+	$scope.openModal = function() {
+    $ionicModal.fromTemplateUrl('app/login/modal.html', {
+      scope: $scope,
+      animation: 'fade-in-scale'
+	    }).then(function(modal) {
+	      $scope.modal = modal;
+	      $scope.modal.show();
+	    });
+	  };
+	  $scope.closeModal = function() {
+	    $scope.modal.hide();
+	  };
+	  //Cleanup the modal when we're done with it!
+	  $scope.$on('$destroy', function() {
+	    $scope.modal.remove();
+	  });
+	  // Execute action on hide modal
+	  $scope.$on('modal.hidden', function() {
+	    // Execute action
+	  });
+	  // Execute action on remove modal
+	  $scope.$on('modal.removed', function() {
+	    // Execute action
+	  });
+}
